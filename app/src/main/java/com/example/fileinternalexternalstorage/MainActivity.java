ec1b645ca3fe6a5b3e5a8c3609837677e6c6df87
Rao Moueed Ahmed <moueedrao@gmail.com>
2019-03-13 12:25:41 +0500

    Add: Helper methods added 1. File getDocumentDir(String name) 2. boolean isExternalStorageWritable() 3. boolean isExternalStorageReadable()

--- app/src/main/java/com/example/fileinternalexternalstorage/MainActivity.java
+++ app/src/main/java/com/example/fileinternalexternalstorage/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.fileinternalexternalstorage;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText readView;
+    EditText writeView;
+
+    FileOutputStream outputStream;
+    FileInputStream inputStream;
+    String filename = "temp.txt";
+
+    File myExternalFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        readView = (EditText) findViewById(R.id.outputEditTextMain);
+        writeView = (EditText) findViewById(R.id.inputEditTextMain);
+
+        Button externalWrite = (Button) findViewById(R.id.writeExternalButtonMain);
+        Button externalRead = (Button) findViewById(R.id.readExternalButtonMain);
+        Button externalDelete = (Button) findViewById(R.id.deleteExternalButtonMain);
+
+        // if not able to write to external storage, disable buttons
+        if (!isExternalStorageWritable() && isExternalStorageReadable()) {
+            externalWrite.setEnabled(false);
+            externalRead.setEnabled(false);
+            externalDelete.setEnabled(false);
+        }
     }
+
+
+    /* Helper Methods */
+
+    public File getDocumentDir(String name) {
+        // Get the directory for the user's public directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS), name);
+
+        return file;
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
 }
